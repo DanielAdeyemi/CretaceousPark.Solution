e04fbc375f6b79515803396e9a603a61775bab82
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-28 20:33:12 -0700

    add seeded data to db

--- CretaceousPark/Models/Animal.cs
+++ CretaceousPark/Models/Animal.cs
@@ -5,7 +5,7 @@ namespace CretaceousPark.Models
     public int AnimalId { get; set; }
     public string Name { get; set; }
     public string Species { get; set; }
-    public int age { get; set; }
+    public int Age { get; set; }
     public string Gender { get; set; }
   }
 }
