df54d12be6ee66d2df3ef3db0151384944b0dbb7
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-28 19:36:32 -0700

    create animals controller for api calls

---
+++ CretaceousPark/Controllers/AnimalsController.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CretaceousPark.Models;
+
+namespace CretaceousPark.Controllers
+{
+  [Route("api/[controller")]
+  [ApiController]
+  public class AnimalsController : ControllerBase
+  {
+    private readonly CretaceousParkContext _db;
+    public AnimalsController(CretaceousParkContext db)
+    {
+      _db = db;
+    }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Animal>>> Get()
+    {
+      return await _db.Animals.ToListAsync();
+    }
+    [HttpPost]
+    public async Task<ActionResult<Animal>> Post(Animal animal)
+    {
+      _db.Animals.Add(animal);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction("Post", new { id = animal.AnimalId}, animal);
+    }
+  }
+}
