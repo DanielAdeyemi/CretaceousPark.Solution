286c5ac3965cb5c637570d706cd17caf8512b1af
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-28 19:09:03 -0700

    create Animal class

---
+++ CretaceousPark/Models/Animal.cs
@@ -0,0 +1,11 @@
+namespace CretaceousPark.Models
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    public string Name { get; set; }
+    public string Species { get; set; }
+    public int age { get; set; }
+    public string Gender { get; set; }
+  }
+}
