ce520a57df8a054c9fa7b47a3c4cda4ccf69e49b
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-28 20:11:24 -0700

    create update functionality for animals

--- CretaceousPark/Controllers/AnimalsController.cs
+++ CretaceousPark/Controllers/AnimalsController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using CretaceousPark.Models;
+using System.Linq;
 
 namespace CretaceousPark.Controllers
 {
@@ -36,6 +37,35 @@ namespace CretaceousPark.Controllers
         return NotFound();
       }
       return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId}, animal);
+    }
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Animal animal)
+    {
+      if(id != animal.AnimalId)
+      {
+        return BadRequest();
+      }
+      _db.Entry(animal).State = EntityState.Modified;
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch(DbUpdateConcurrencyException)
+      {
+        if(!AnimalExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+      return NoContent();
+    }
+    private bool AnimalExists(int id)
+    {
+      return _db.Animals.Any(e => e.AnimalId == id);
     }
   }
 }
